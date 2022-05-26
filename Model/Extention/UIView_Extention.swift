04a725495724793625dec9f56ff39a30e355dc88
gggno <geunhojeong.jgh@gmail.com>
2022-05-27 01:29:32 +0900

    MainViewController Basic Setting

--- Model/Extention/UIView_Extention.swift
+++ Model/Extention/UIView_Extention.swift
@@ -2,6 +2,7 @@ import Foundation
 import UIKit
 
 extension UIView {
+    // 그라데이션 컬러 설정
     func GradientColor(color1: UIColor, color2: UIColor) {
         let gradient: CAGradientLayer = CAGradientLayer()
         gradient.colors = [color1.cgColor, color2.cgColor]
@@ -10,5 +11,11 @@ extension UIView {
         gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
         gradient.frame = bounds
         layer.insertSublayer(gradient, at: 0)
+    }
+    // 뷰 부분적 cornerRadius 설정
+    func CornerRadiusLayerSetting(cornerRadius: CGFloat, cornerLayer: CACornerMask) {
+        clipsToBounds = true
+        layer.cornerRadius = cornerRadius
+        layer.maskedCorners = CACornerMask(arrayLiteral: cornerLayer)
     }
 }
--- View/MainViewController.swift
+++ View/MainViewController.swift
@@ -2,9 +2,21 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    //MARK: - IBOutlet
+    @IBOutlet weak var tableBackgroundView: UIView!
+    @IBOutlet weak var settingBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        view.GradientColor(color1: UIColor(named: "MainYellowColor")!, color2: UIColor(named: "MainOrangeColor")!)
+        
+        tableBackgroundView.CornerRadiusLayerSetting(cornerRadius: 60, cornerLayer: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
     }
+    
+    @IBAction func settingBtnClicked(_ sender: Any) {
+        
+    }
+    
     
 }
