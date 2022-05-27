3af8150a06478b5632574cbb39d6365a8560e963
gggno <geunhojeong.jgh@gmail.com>
2022-05-27 16:55:57 +0900

    MainTableView frame setting completed

---
+++ View/MainTableViewCell.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+    
+    //MARK: - IBOutlet
+    @IBOutlet weak var SubjectNameLabel: UILabel!
+    @IBOutlet weak var ProfessorNameLabel: UILabel!
+    @IBOutlet weak var NumberOfParticipantsLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- View/MainViewController.swift
+++ View/MainViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     //MARK: - IBOutlet
     @IBOutlet weak var tableBackgroundView: UIView!
     @IBOutlet weak var settingBtn: UIButton!
+    @IBOutlet weak var MainTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,7 +13,22 @@ class MainViewController: UIViewController {
         view.GradientColor(color1: UIColor(named: "MainYellowColor")!, color2: UIColor(named: "MainOrangeColor")!)
         
         tableBackgroundView.CornerRadiusLayerSetting(cornerRadius: 60, cornerLayer: [.layerMinXMinYCorner, .layerMaxXMinYCorner])
+        
+        MainTableView.delegate = self
+        MainTableView.dataSource = self
+        MainTableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "MainTableViewCell")
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 7 // 임시 코드
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath)
+        cell.layer.cornerRadius = 50
+        return cell
+    }
+    
     
     @IBAction func settingBtnClicked(_ sender: Any) {
         
@@ -20,3 +36,4 @@ class MainViewController: UIViewController {
     
     
 }
+
