db8e592af9a9fb5093b9236322f8570229d8e6d5
gggno <geunhojeong.jgh@gmail.com>
2022-08-04 22:49:15 +0900

    디비, 테이블생성 작업

---
+++ Model/Entity/Sql.swift
@@ -0,0 +1,117 @@
+import Foundation
+import SQLite3
+
+class Sql {
+    var db: OpaquePointer? = nil
+    
+    init() {
+        self.db = createDB()
+    }
+    
+    // 디비 생성
+    func createDB() -> OpaquePointer? {
+        
+        let filePath = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("NyangVillageTable.sqlite")
+        
+        var db: OpaquePointer? = nil
+        
+        if sqlite3_open(filePath.path, &db) != SQLITE_OK {
+            print("Error while creating db")
+            return nil
+        } else {
+            print("Database has been created with path NyangVillageTableTable.sqlite")
+            return db
+        }
+    }
+    
+    // 테이블 생성
+    
+    func createUserInfoTable() {
+        
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS UserInfo (studentid TEXT, token TEXT, suspendeddate TEXT, autologin INTEGER, jwt TEXT)"
+        
+        var createTablePtr: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(self.db, createTableQuery, -1, &createTablePtr, nil) == SQLITE_OK {
+            
+            if sqlite3_step(createTablePtr) == SQLITE_DONE {
+                
+                print("UserInfoTable creation has been successfully done")
+            }
+            else {
+                print("UserInfoTable creation failure")
+            }
+        } else {
+            print("Preparation for creating UserInfoTable has been failed")
+        }
+        
+        sqlite3_finalize(createTablePtr)
+    }
+    
+    func createRoomInfoTable() {
+        
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS RoomInfo (roomid TEXT, roomname TEXT, nickname TEXT, professorname TEXT, position INTEGER, noti INTEGER)"
+        
+        var createTablePtr: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(self.db, createTableQuery, -1, &createTablePtr, nil) == SQLITE_OK {
+            
+            if sqlite3_step(createTablePtr) == SQLITE_DONE {
+                
+                print("RoomInfoTable creation has been successfully done")
+            }
+            else {
+                print("RoomInfoTable creation failure")
+            }
+        } else {
+            print("Preparation for creating RoomInfoTable has been failed")
+        }
+        
+        sqlite3_finalize(createTablePtr)
+    }
+    
+    func createRoomInNameTable() {
+        
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS RoomInName (id INTEGER, roomid INTEGER, name TEXT)"
+        
+        var createTablePtr: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(self.db, createTableQuery, -1, &createTablePtr, nil) == SQLITE_OK {
+            
+            if sqlite3_step(createTablePtr) == SQLITE_DONE {
+                
+                print("RoomInNameTable creation has been successfully done")
+            }
+            else {
+                print("RoomInNameTable creation failure")
+            }
+        } else {
+            print("Preparation for creating RoomInNameTable has been failed")
+        }
+        
+        sqlite3_finalize(createTablePtr)
+    }
+    
+    func createChatInfoTable() {
+        
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS ChatInfo (id INTEGER, roomid INTEGER, nickname TEXT, time TEXT, content TEXT, type INTEGER)"
+        
+        var createTablePtr: OpaquePointer? = nil
+        
+        if sqlite3_prepare_v2(self.db, createTableQuery, -1, &createTablePtr, nil) == SQLITE_OK {
+            
+            if sqlite3_step(createTablePtr) == SQLITE_DONE {
+                
+                print("ChatInfoTable creation has been successfully done")
+            }
+            else {
+                print("ChatInfoTable creation failure")
+            }
+        } else {
+            print("Preparation for creating ChatInfoTable has been failed")
+        }
+        
+        sqlite3_finalize(createTablePtr)
+    }
+    
+}
