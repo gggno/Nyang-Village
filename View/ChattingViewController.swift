79264d8a6e3db6308d0e7518eb1d1e3d411579ba
gggno <geunhojeong.jgh@gmail.com>
2022-08-14 14:48:00 +0900

    채팅방 내비게이션 아이템 구성

--- View/ChattingViewController.swift
+++ View/ChattingViewController.swift
@@ -1,13 +1,20 @@
 import UIKit
 
 class ChattingViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        self.navigationController?.navigationBar.topItem?.title = ""
+        self.navigationController?.navigationBar.tintColor = .black
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "line.3.horizontal"), style: .plain, target: self, action: #selector(rightBtnClicked))
+        
         
     }
+        
+    @objc func rightBtnClicked() {
+        print("click")
+    }
     
-
 }
