4ae0f045e441e88fa539a9f0db5e594558488f00
gggno <geunhojeong.jgh@gmail.com>
2022-08-14 15:30:34 +0900

    채팅방 내비게이션 수정

--- View/ChattingViewController.swift
+++ View/ChattingViewController.swift
@@ -5,8 +5,13 @@ class ChattingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.GradientColor(color1: UIColor(named: "MainYellowColor")!, color2: UIColor(named: "MainOrangeColor")!)
+        
+        self.title = "냥빌리지 프로젝트 01반"
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
         self.navigationController?.navigationBar.topItem?.title = ""
-        self.navigationController?.navigationBar.tintColor = .black
+        self.navigationController?.navigationBar.tintColor = .white
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "line.3.horizontal"), style: .plain, target: self, action: #selector(rightBtnClicked))
         
--- View/SettingViewController.swift
+++ View/SettingViewController.swift
@@ -7,6 +7,8 @@ class SettingViewController: UIViewController {
 
         self.navigationController?.navigationBar.topItem?.title = ""
         self.navigationController?.navigationBar.tintColor = .black
+        
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black]
         self.title = "설정"
         
     }
