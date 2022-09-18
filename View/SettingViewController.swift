0800de29d23dec5bf56c39207e74ff11ba53bf33
gggno <geunhojeong.jgh@gmail.com>
2022-09-18 20:57:47 +0900

    설정 테이블 뷰 적용

--- View/SettingViewController.swift
+++ View/SettingViewController.swift
@@ -1,7 +1,10 @@
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // MARK: - IBOutlet
+    @IBOutlet weak var settingTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -11,6 +14,19 @@ class SettingViewController: UIViewController {
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.black]
         self.title = "설정"
         
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+        settingTableView.register(UINib(nibName: "SettingTableViewCell", bundle: nil), forCellReuseIdentifier: "SettingTableViewCell")
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingTableViewCell", for: indexPath) as! SettingTableViewCell
+        
+        return cell
     }
     
 }
