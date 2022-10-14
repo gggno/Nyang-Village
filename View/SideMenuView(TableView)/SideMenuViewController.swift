7d4f6503e2086f2259fe8f25bf8451dca66483b6
gggno <geunhojeong.jgh@gmail.com>
2022-10-14 16:34:15 +0900

    알림 허용 해제 기능 구현 중

--- View/SideMenuView(TableView)/SideMenuViewController.swift
+++ View/SideMenuView(TableView)/SideMenuViewController.swift
@@ -4,11 +4,11 @@ class SideMenuViewController: UIViewController, UITableViewDelegate, UITableView
     
     // MARK: - IBOutlet
     @IBOutlet weak var sideMenuTableView: UITableView!
-   
+    
     @IBOutlet weak var subjectNameLabel: UILabel!
     @IBOutlet weak var professorNameLabel: UILabel!
     @IBOutlet weak var bellBtn: UIButton!
-  
+    
     var subjectName: String?
     var professorName: String?
     var roomId: Int?
@@ -38,15 +38,21 @@ class SideMenuViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     override func viewWillAppear(_ animated: Bool) {
-            super.viewWillAppear(animated)
-            
-            navigationController?.setNavigationBarHidden(true, animated: animated)
-        }
-
-        override func viewWillDisappear(_ animated: Bool) {
-            super.viewWillDisappear(animated)
-            navigationController?.setNavigationBarHidden(false, animated: animated)
-        }
+        print("SideMenuViewController viewWillAppear() called")
+        
+        // if sql문에 노티가 1, 0 일때의 이미지 표현
+        
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("SideMenuViewController viewWillDisappear() called")
+        
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return roomInNames.count
@@ -63,6 +69,11 @@ class SideMenuViewController: UIViewController, UITableViewDelegate, UITableView
     
     // 알림 이미지 클릭했을 때
     @IBAction func bellClicked(_ sender: Any) {
+        print("bell clicked")
+        
+        // if 노티가 1이면 0(알림 허용 -> 해제) 1. sql문에 노티 변경 2. 벨 이미지 변경, 노티가 1이면 0(알림 해제 -> 허용) 1. sql문에 노티 변경 2. 벨 이미지 변경
+        
+        bellBtn.setImage(UIImage(systemName: "bell.slash.fill"), for: .normal)
     }
     
     
