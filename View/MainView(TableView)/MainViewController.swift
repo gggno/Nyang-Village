f4398af860f9801429e89030a190fc5df76639e6
gggno <geunhojeong.jgh@gmail.com>
2022-10-19 22:55:43 +0900

    코드 정리

--- View/MainView(TableView)/MainViewController.swift
+++ View/MainView(TableView)/MainViewController.swift
@@ -31,7 +31,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         MainTableView.delegate = self
         MainTableView.dataSource = self
         MainTableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "MainTableViewCell")
-        print("main roomId= \(roomId)")
+
         if roomId != nil { // 푸시 알림을 클릭했을 때(roomId에 값이 있을 때)
             print("푸시 알림을 클릭했을 때(roomId에 값이 있을 때)")
             let chatVC = self.storyboard?.instantiateViewController(withIdentifier: "ChattingViewController") as! ChattingViewController
